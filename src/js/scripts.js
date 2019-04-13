a18287201c8c9f74e1345ac55db015fc1242c9ab
Alex Zappa <reatlat@gmail.com>
2019-04-13 02:01:37 +0200

    update js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -92,16 +92,28 @@
         _initTriggers() {
             this._debugLog('_initTriggers()');
 
+            $('#ami-share-buttons').on('click', function (e) {
+                $('.addthis_toolbox_wrapper').toggleClass('active');
+            });
             $('#domain').donetyping(function () {
                 window.AMI._debugLog('donetiping');
-                let url = $('#domain').val();
+                let url = window.AMI._checkUrl( $('#domain').val() );
 
-                $("iframe").attr('src', window.AMI._checkUrl(url));
+                // update url address bar
+                window.history.pushState(null,null, location.origin + '/?url=' + url);
+
+                // update addThis @link https://www.addthis.com/academy/
+                window.addthis_share.url = window.location.href;
+                window.addthis_share.title = window.document.title + " - " + url;
+                window.addthis.toolbox('.addthis_toolbox');
+
+                $("iframe").attr('src', url);
             }, 1000);
 
             if (this._GET('url')) {
+                window.AMI._debugLog('Use URL params');
                 let url = window.AMI._checkUrl(this._GET('url'));
-                $('#domain-input-wrapper').addClass('is-dirty ');
+                $('#domain-input-wrapper').addClass('is-dirty');
                 $('#domain').val(url);
                 $("iframe").attr('src', url);
             }
